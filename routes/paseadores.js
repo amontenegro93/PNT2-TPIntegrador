e1a473158d7dd5621f6be48d8ca3065882a64dc1
amontenegro93 <agustin_montenegro@outlook.com>
2022-05-23 22:34:58 -0300

    mas test

--- routes/paseadores.js
+++ routes/paseadores.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Paseadores. */
+/* GET Lista paseadores. */
 router.get('/', function(req, res, next) {
-  //debe devolver una lista de paseadores
+  let paseadores = [{id:1234},{id:5678}]
+  res.json(paseadores)
+  res.send(paseadores);
 });
+
+router.get('/:id', function(req, res, next){
+  let paseadores = [{id:1234},{id:5678}]
+
+})
+
+router.post('/', function(req, res, next){
+  
+})
+
+router.delete('/', function(req, res, next){
+  
+})
+
+router.put('/', function(req, res, next){
+  
+})
+
+router.patch('/', function(req, res, next){
+  
+})
+
 
 module.exports = router;
--- test/testPaseadores.js
+++ test/testPaseadores.js
@@ -4,18 +4,46 @@ const app = require('../app');
 const axios = require('axios')
 const paseadores = require('../routes/paseadores');
 
-describe('test', ()=>{
-  describe('Server', ()=>{
-    it('server is on', async ()=>{
-      const response = await axios.get('http://localhost:3000/')
-      expect(response.status).to.equal(200);
-    })
-
-    it('server is on', async ()=>{
+describe('paseadores', ()=>{
+  describe('GET paseadores', ()=>{
+    it('trae lista paseadores', async ()=>{
       //preguntar a paseadores en vez de axios.get
-      const response = await axios.get('http://localhost:3000/paseadores')
-      expect(response.status).to.equal(200);
+      const response = await axios.get('http://localhost:3000/paseadores');
+      expect(response.data).to.eql([{id:1234},{id:5678}])
     })
 
   })
+
+    describe('GET paseadores/:id', ()=>{
+      it('trae un paseador por id', async ()=>{
+        const response = await axios.get('http://localhost:3000/paseadores/1234');
+        expect(response.data).to.eql([{id:1234}])
+      })
+
+    })
+
+    describe('POST/paseadores', () =>{
+      it('crea  un paseador', ()=>{
+          
+      })
+    })
+
+    describe('DELETE/paseadores', () =>{
+        it('borra un paseador', ()=>{
+            
+        })
+      })
+
+    describe('PATCH/paseadores', () =>{
+      it('edita un paseador', ()=>{
+          
+      })
+    })
+
+    describe('PUT/paseadores', () =>{
+      it('edita un paseador', ()=>{
+          
+      })
+    })
+
 });
