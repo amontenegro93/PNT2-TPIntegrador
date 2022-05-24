26ee870ba364fcf3060bd1e207ff62e4b2fa6130
amontenegro93 <agustin_montenegro@outlook.com>
2022-05-23 22:50:32 -0300

    crear gitignore y duenios

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var paseadoresRouter = require('./routes/paseadores');
+var dueniosRouter = require('./routes/duenios');
+
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/paseadores', paseadoresRouter);
+app.use('/duenios', dueniosRouter);
+
 
 
 // catch 404 and forward to error handler
